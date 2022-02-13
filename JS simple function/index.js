27dadeb0f822d5c4e37335bc2355921a330eb1ab
vxrossa <ibasalaev@icloud.com>
2022-02-13 22:41:05 +0300

    function refactor

--- JS simple function/index.js
+++ JS simple function/index.js
@@ -1,34 +1,7 @@
-export function func(s, a, b) {
-
-    if (s.match(/^$/)) {
-        return -1;
-    }
-
-    var i = s.length - 1;
-    var aIndex = -1;
-    var bIndex = -1;
-
-    while ((aIndex == -1) && (bIndex == -1) && (i > 0)) {
-        if (s.substring(i, i + 1) == a) {
-            aIndex = i;
-        }
-        if (s.substring(i, i + 1) == b) {
-            bIndex = i;
-        }
-        i = i - 1;
-    }
-
-    if (aIndex != -1) {
-        if (bIndex == -1) {
-            return aIndex;
-        } else {
-            return Math.max(aIndex, bIndex);
-        }
-    }
-
-    if (bIndex != -1) {
-        return bIndex;
-    } else {
-        return -1;
-    }
+const largestIndexOfLetters = (string, ...letters) => {
+  const indexesArr = [ ...letters ]
+    .map((letter) => letter.length === 1 ? string.lastIndexOf(letter) : -1);
+  return Math.max(...indexesArr);
 }
+
+module.exports = largestIndexOfLetters;
