864cec688adac6b3e54a0e753f7a67d026778bdb
Yury Babaev <yurybabaev@gmail.com>
2022-03-31 11:16:16 +0300

    Refactored func in index.js

--- JS simple function/index.js
+++ JS simple function/index.js
@@ -1,34 +1,27 @@
+/**
+ * 
+ * @param {string} s string to test
+ * @param {any} a first param
+ * @param {any} b second param
+ * @returns Last index of either a or b in s (what appears rightmost). Equality is
+ * determined using non-strict comparison, on a per-char basis. If the last index of a or b
+ * is 0 or no match has been found, -1 is returned
+ */
 export function func(s, a, b) {
 
-    if (s.match(/^$/)) {
+    if (s === "" ||
+        a && a.length === 0 || // Optimize for arguments with length > 1,
+        b && b.length === 0) { // they all will take O(n)
         return -1;
     }
 
-    var i = s.length - 1;
-    var aIndex = -1;
-    var bIndex = -1;
-
-    while ((aIndex == -1) && (bIndex == -1) && (i > 0)) {
-        if (s.substring(i, i + 1) == a) {
-            aIndex = i;
+    let i = s.length - 1;
+    while (i > 0) {
+        if (s[i] == a || s[i] == b) {
+            return i;
         }
-        if (s.substring(i, i + 1) == b) {
-            bIndex = i;
-        }
-        i = i - 1;
+        i -= 1;
     }
 
-    if (aIndex != -1) {
-        if (bIndex == -1) {
-            return aIndex;
-        } else {
-            return Math.max(aIndex, bIndex);
-        }
-    }
-
-    if (bIndex != -1) {
-        return bIndex;
-    } else {
-        return -1;
-    }
+    return -1;
 }
