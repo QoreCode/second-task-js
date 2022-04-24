6095a8099ea7210536af66e77a0451c419f3e787
sergeytokarev <stokarev@novakidschool.com>
2022-04-24 21:00:07 +0700

    01-js - simplify

--- JS simple function/src/index.js
+++ JS simple function/src/index.js
@@ -8,38 +8,17 @@
  */
 function func(s, a, b) {
 
-    // слишком жирно проверять пустоту строки через regex
     if (s.length === 0) {
         return -1;
     }
 
     let i = s.length - 1;
-    let aIndex = -1;
-    let bIndex = -1;
-
-    while ((aIndex === -1) && (bIndex === -1) && (i > 0)) {
-        if (s.substring(i, i + 1) === a) {
-            aIndex = i;
-        }
-        if (s.substring(i, i + 1) === b) {
-            bIndex = i;
-        }
-        i = i - 1;
+    while (i > 0) {
+        if (s[i] === a) return i;
+        if (s[i] === b) return i;
+        i--;
     }
-
-    if (aIndex !== -1) {
-        if (bIndex === -1) {
-            return aIndex;
-        } else {
-            return Math.max(aIndex, bIndex);
-        }
-    }
-
-    if (bIndex != -1) {
-        return bIndex;
-    } else {
-        return -1;
-    }
+    return -1;
 }
 
 module.exports = {
