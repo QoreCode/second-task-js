245333b143b4056c7d0203683c6e77b4742d9d35
sergeytokarev <stokarev@novakidschool.com>
2022-04-24 21:27:52 +0700

    02-ts - Promise.all

--- TS OOP/src/app/index.ts
+++ TS OOP/src/app/index.ts
@@ -13,8 +13,16 @@ async function user1ClientFlow(): Promise<void> {
   const house1 = HouseCreator.createClassicHouse(2);
   const house2 = HouseCreator.createModernHouse(4);
 
-  await connection.save(house1);
-  await connection.save(house2);
+  /**
+   * Тут надо проконсультироваться с бэком,
+   * не возражает ли он против одновременных запросов,
+   * обычно такой вариант ускоряет работу приложения,
+   * сокращая время ожидания
+   */
+  await Promise.all([
+    connection.save(house1),
+    connection.save(house2),
+  ])
 
   console.log(`user1 house1`, house1);
   console.log(`user1 house2`, house2);
@@ -39,9 +47,11 @@ async function user2ClientFlow(): Promise<void> {
 
   const house3 = HouseCreator.createClassicHouse(3);
 
-  await connection.save(house1);
-  await connection.save(house2);
-  await connection.save(house3);
+  await Promise.all([
+    connection.save(house1),
+    connection.save(house2),
+    connection.save(house3),
+  ])
 
   console.log(`user2 house1`, house1);
   console.log(`user2 house2`, house2);
