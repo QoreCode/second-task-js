f866730739dbb4397d1e3d5d4744d69e75c00ed2
spoil349 <spoil349@mail.ru>
2022-04-25 00:05:18 +0400

    refactor: self refactoring for 'JS simple function'

--- js-simple-function/index.js
+++ js-simple-function/index.js
@@ -1,25 +1,38 @@
-export function func(s, a, b) {
+export function func(testingString, firstLetter, secondLetter) {
 
-    if(s.length <= 0) {
+    // получилось два решения, одно - закомментированное
+    // второе - после того как узнал про метод lastIndexOf
+    // оба рабочи
+
+    // if(testingString.length <= 0) {
+    //     return -1;
+    // }
+
+    // let strArr = Array.from(testingString);
+    // let firstLetterLastIndex = 0;
+    // let secondLetterLastIndex = 0;
+
+    // strArr.forEach((letter, index) => {
+    //     if (letter === firstLetter) {
+    //         firstLetterLastIndex = index;
+    //     }
+    //     if (letter === secondLetter) {
+    //         secondLetterLastIndex = index;
+    //     }
+    // });
+
+    // if (firstLetterLastIndex === 0 && secondLetterLastIndex === 0) {
+    //     return -1;
+    // }
+
+    // return firstLetterLastIndex > secondLetterLastIndex ? firstLetterLastIndex : secondLetterLastIndex;
+
+    if (testingString.length === 0 | firstLetter?.length > 1 || secondLetter?.length > 1) {
         return -1;
     }
 
-    let strArr = Array.from(s);
-    let aIndex = 0;
-    let bIndex = 0;
+    const firstLetterLastIndex = testingString.lastIndexOf(firstLetter);
+    const secondLetterLastIndex = testingString.lastIndexOf(secondLetter);
     
-    strArr.forEach((letter, index) => {
-        if (letter === a) {
-            aIndex = index;
-        }
-        if (letter === b) {
-            bIndex = index;
-        }
-    });
-
-    if (aIndex === 0 && bIndex === 0) {
-        return -1;
-    }
-
-    return aIndex > bIndex ? aIndex : bIndex;
+    return firstLetterLastIndex > secondLetterLastIndex ? firstLetterLastIndex : secondLetterLastIndex;
 }
