a5edec5069e80d47465cdded31825f392b0e330d
sergeytokarev <stokarev@novakidschool.com>
2022-04-24 20:24:58 +0700

    01-js - regex, let && ===

--- JS simple function/src/index.js
+++ JS simple function/src/index.js
@@ -1,25 +1,26 @@
 function func(s, a, b) {
 
-    if (s.match(/^$/)) {
+    // слишком жирно проверять пустоту строки через regex
+    if (s.length === 0) {
         return -1;
     }
 
-    var i = s.length - 1;
-    var aIndex = -1;
-    var bIndex = -1;
+    let i = s.length - 1;
+    let aIndex = -1;
+    let bIndex = -1;
 
-    while ((aIndex == -1) && (bIndex == -1) && (i > 0)) {
-        if (s.substring(i, i + 1) == a) {
+    while ((aIndex === -1) && (bIndex === -1) && (i > 0)) {
+        if (s.substring(i, i + 1) === a) {
             aIndex = i;
         }
-        if (s.substring(i, i + 1) == b) {
+        if (s.substring(i, i + 1) === b) {
             bIndex = i;
         }
         i = i - 1;
     }
 
-    if (aIndex != -1) {
-        if (bIndex == -1) {
+    if (aIndex !== -1) {
+        if (bIndex === -1) {
             return aIndex;
         } else {
             return Math.max(aIndex, bIndex);
@@ -33,6 +34,6 @@ function func(s, a, b) {
     }
 }
 
-module.exports =  {
+module.exports = {
     func
 }
